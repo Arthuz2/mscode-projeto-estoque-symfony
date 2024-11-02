7610a12d33fcde9482f0ef4b010a38a4eb6143f8
isaac <isaactiagowandermure.com>
2024-11-01 22:08:51 -0300

    começando a implementa a logica de finalizar a venda

--- src/Controller/Venda/NovaVendaController.php
+++ src/Controller/Venda/NovaVendaController.php
@@ -2,11 +2,11 @@
 
 namespace App\Controller\Venda;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+/* use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Annotation\Route; */
 
-class NovaVendaController extends AbstractController
+/* class NovaVendaController extends AbstractController
 {
 
     #[Route("/novaVenda", name:"novaVenda")]
@@ -14,5 +14,31 @@ class NovaVendaController extends AbstractController
     {
         return $this->render("venda/novaVenda.html.twig");
     }
-}
+}  */
+
+use App\Repository\CarrinhoRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NovaVendaController extends AbstractController
+{
+    #[Route('/novaVenda', name: 'nova_venda')]
+    public function novaVenda( CarrinhoRepository $carrinhoRepository): Response
+    {
+      
+        $carrinho = $carrinhoRepository->find(2);
+
+        // Verifica se o carrinho existe
+        if (!$carrinho) {
+            throw $this->createNotFoundException('Carrinho não encontrado.');
+        }
+
+        // Passa o carrinho para o template
+        return $this->render('venda/novaVenda.html.twig', [
+            'carrinho' => $carrinho,
+        ]);
+    }
+} 
+
 ?>
---
+++ src/Controller/Venda/finalizarVendaController.php
@@ -0,0 +1,57 @@
+<?php 
+
+
+namespace App\Controller\Venda;
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+use App\Repository\CarrinhoRepository;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class finalizarVendaController extends AbstractController
+{
+    private CarrinhoRepository $carrinhoRepository;
+
+    public function __construct(
+        CarrinhoRepository $carrinhoRepository
+    )
+    {
+        $this->carrinhoRepository = $carrinhoRepository;
+    }
+
+    #[Route("/venda/{id}/finalizar", name: "finalizarVenda", methods: ["POST"])]
+    function finalizarVenda(int $id): Response
+    {
+
+         // Valida se o carrinho existe
+         $carrinho = $this->carrinhoRepository->find($id);
+
+      
+         if (!$carrinho) {
+             throw  $this->createNotFoundException('Carrinho não encontrado.');
+         }
+ 
+         // Valida se o carrinho contém produtos
+         if (count($carrinho->getProdutos()) === 0) {
+             throw new BadRequestHttpException('O carrinho não contém produtos.');
+         }
+ 
+         // Valida se o carrinho já foi finalizado
+         if ($carrinho->getStatus() !== 'pendente') {
+             throw new BadRequestHttpException('Não é possível finalizar um carrinho que não está pendente.');
+         }
+ 
+         // Altera o status para "aguardando pagamento"
+         $carrinho->setStatus('aguardando pagamento');
+         $this->carrinhoRepository->save($carrinho);
+
+    
+ 
+        return new Response('Carrinho alterado para aguardando pagamento.', Response::HTTP_OK);
+    }
+}
+
+?>
