646066a3a48e43601eb868b316f41aa928f62212
Arthuz2 <arthur.porcino43@gmail.com>
2024-11-11 19:36:18 -0300

    alimentando o select

--- src/Controller/Venda/FinalizarVendaController.php
+++ src/Controller/Venda/FinalizarVendaController.php
@@ -1,30 +1,32 @@
 <?php 
 
 namespace App\Controller\Venda;
-use App\Entity\Carrinho;
+use App\Repository\CarrinhoRepository;
 use App\Service\FinalizarVendaService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FinalizarVendaController extends AbstractController
 {
-    #[Route("novaVenda/finalizarCarrinho/{carrinho}", name: "finalizarCarrinho", methods:"POST")]
+    #[Route("novaVenda/finalizarCarrinho", name: "finalizarCarrinho", methods:"POST")]
     public function finalizarVenda(
-        Carrinho $carrinho,
-        FinalizarVendaService $finalizarVendaService
-    ): Response
+        Request $request,
+        FinalizarVendaService $finalizarVendaService,
+        CarrinhoRepository $carrinhoRepository
+    ): JsonResponse
     {
+        $id = $request->request->get('cliente');
         try {
+            $carrinho = $carrinhoRepository->findBy(['id' => $id])[0];
             $finalizarVendaService->execute(carrinho: $carrinho);
             return new JsonResponse([
                 "message" => 'Carrinho alterado para aguardando pagamento.'
             ]);
 
         }catch (\Throwable $e) {
-            // Retorna uma resposta JSON em caso de erro
-            return new JsonResponse(['error' => $e->getMessage()],$e->getCode());
+            return new JsonResponse(['error' => $e->getMessage()]);
         }
     }
 }
