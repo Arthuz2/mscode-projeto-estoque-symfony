9698dbab92beacf04e373d9496a5809be511c551
isaactiago <141565649+isaactiago@users.noreply.github.com>
2024-11-28 20:52:01 -0300

    Update src/Controller/Exceptions/ProdutoJaAdicionadoAoCarrinhoException.php
    Co-authored-by: Rejman Nascimento <72097727+rejmann@users.noreply.github.com>

--- src/Controller/Exceptions/ProdutoJaAdicionadoAoCarrinhoException.php
+++ src/Controller/Exceptions/ProdutoJaAdicionadoAoCarrinhoException.php
@@ -2,10 +2,10 @@
 
 namespace App\Controller\Exceptions;
 
-class  ProdutoJaAdicionadoAoCarrinhoException extends \Exception
+class ProdutoJaAdicionadoAoCarrinhoException extends \Exception
 {
     public function __construct()
     {
-        parent::__construct("Produto ja adicionado ao carrinho .");
+        parent::__construct('Produto ja adicionado ao carrinho.');
     }
 } 
